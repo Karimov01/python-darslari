fd2b526dd06032db4977a353cc41fcabac92232f
KAMOLIDDIN KARIMOV <110274426+Karimov01@users.noreply.github.com>
2022-07-30 10:34:28 +0500

    Create 1-dars.py

---
+++ 1-dars.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# yosh = int(input("Yoshingiz nechida?" )) 
+# if yosh <=5:
+#    narh = 0
+# elif yosh <= 15:
+#    narh = 5000
+# else: 
+#    narh = 15000
+    
+# print (f"sizga kirish {narh} so'm" )  
+
+
+# kun = input ("bugun nima kun? >>>") 
+# if kun.lower() =='shanba' or kun.lower() == 'yakshanba':
+#     print ("Bugun dam olish kuni.")
+# else:
+#     print ("Bugun Ish Kuni.")
+
+
+#kun = input ("Bugun qanday kun? ")
+#harorat = float(input("Havo harorati qanday?")) 
+
+#if kun.lower () == "payshanba" and harorat>=30:
+#    print ("cho'milgani ketdik!")
+#elif kun.lower()== "payshanba" and harorat<30:     
+#    print ("Uyda dam olamiz ✻ ")
+
+
+son = input ('istalgan son kiriting')
+son = int (son)
+print (f'{son} ning kvadrati {son**2} ga teng')
